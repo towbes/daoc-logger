30e67fe74e018d8fd97c342ec7bc24b95494d506
towbes <hank.wyatt@gmail.com>
2022-08-07 21:15:08 +0900

    fix if

--- daoc-logger/dllmain.cpp
+++ daoc-logger/dllmain.cpp
@@ -408,7 +408,7 @@ DWORD WINAPI WindowThread(HMODULE hModule){
 
     char* name = (char*)(moduleBase + 0xc4a6a8);
     std::cout << "Server name is: " << name[0] << std::endl;
-    if (name[0] != 'G' || name[0] != 'D') {
+    if (name[0] != 'G' && name[0] != 'D') {
 #ifdef _DEBUG
         if (f != 0) {
             fclose(f);
